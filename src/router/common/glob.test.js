1c3be6f4b03061f65ffbc7ce3e3f0430861a8e8f
Anatoly Ostrovsky <anatolyostrovsky@gmail.com>
2024-07-11 00:08:48 +0300

    Fix test

--- src/router/common/glob.test.js
+++ src/router/common/glob.test.js
@@ -1,6 +1,6 @@
 import { test, expect } from "@playwright/test";
 
-const TEST_URL = "src/router/common/glob.spec.js";
+const TEST_URL = "src/router/common/glob.html";
 
 test("unit tests contain no errors", async ({ page }) => {
   await page.goto(TEST_URL);
