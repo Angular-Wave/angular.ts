39e3d04df904858813811a4c219c68f8e7e129a5
Anatoly Ostrovsky <anatolyostrovsky@gmail.com>
2024-07-26 17:09:46 +0300

    Finalize IF directive animations

--- src/directive/if/if.test.js
+++ src/directive/if/if.test.js
@@ -11,61 +11,104 @@ test("unit tests contain no errors", async ({ page }) => {
   );
 });
 
-test("animations", async ({ page }) => {
-  await page.goto("src/directive/if/if-animate-css.html");
-  await page.content();
-  // when if attribute has `data-animate` set to true
-  await page.click('button:has-text("Fade In!")');
-  let animated = await page.locator("#data-animate");
+test.describe("animations", () => {
+  test("it should add enter and leave classes when animation is enabled", async ({
+    page,
+  }) => {
+    await page.goto("src/directive/if/if-animate-css.html");
+    await page.content();
+    // when if attribute has `data-animate` set to true
+    await page.click('button:has-text("Fade In!")');
+    let animated = await page.locator("#data-animate");
 
-  await expect(animated).toBeVisible();
-  await expect(animated).toHaveClass(/ng-enter/);
-  await expect(animated).toHaveClass(/ng-enter-active/);
+    await expect(animated).toBeVisible();
+    await expect(animated).toHaveClass(/ng-enter/);
+    await expect(animated).toHaveClass(/ng-enter-active/);
 
-  // Wait for the transition to complete
-  await page.waitForTimeout(500);
-  await expect(animated).not.toHaveClass(/ng-enter/);
-  await expect(animated).not.toHaveClass(/ng-enter-active/);
+    // Wait for the transition to complete
+    await page.waitForTimeout(500);
+    await expect(animated).not.toHaveClass(/ng-enter/);
+    await expect(animated).not.toHaveClass(/ng-enter-active/);
 
-  await page.click('button:has-text("Fade Out!")');
-  await expect(animated).toHaveClass(/ng-leave/);
-  await expect(animated).toHaveClass(/ng-leave-active/);
-  await page.waitForTimeout(500);
-  // should be invisible
-  await expect(animated).not.toBeVisible();
+    await page.click('button:has-text("Fade Out!")');
+    await expect(animated).toHaveClass(/ng-leave/);
+    await expect(animated).toHaveClass(/ng-leave-active/);
+    await page.waitForTimeout(500);
+    // should be invisible
+    await expect(animated).not.toBeVisible();
 
-  // when if attribute has `animate` set to true
-  await page.click('button:has-text("Fade In!")');
-  animated = await page.locator("#animate");
+    // when if attribute has `animate` set to true
+    await page.click('button:has-text("Fade In!")');
+    animated = await page.locator("#animate");
 
-  await expect(animated).toBeVisible();
-  await expect(animated).toHaveClass(/ng-enter/);
-  await expect(animated).toHaveClass(/ng-enter-active/);
+    await expect(animated).toBeVisible();
+    await expect(animated).toHaveClass(/ng-enter/);
+    await expect(animated).toHaveClass(/ng-enter-active/);
 
-  // Wait for the transition to complete
-  await page.waitForTimeout(500);
-  await expect(animated).not.toHaveClass(/ng-enter/);
-  await expect(animated).not.toHaveClass(/ng-enter-active/);
+    // Wait for the transition to complete
+    await page.waitForTimeout(500);
+    await expect(animated).not.toHaveClass(/ng-enter/);
+    await expect(animated).not.toHaveClass(/ng-enter-active/);
 
-  await page.click('button:has-text("Fade Out!")');
-  await expect(animated).toHaveClass(/ng-leave/);
-  await expect(animated).toHaveClass(/ng-leave-active/);
-  await page.waitForTimeout(500);
-  // should be invisible
-  await expect(animated).not.toBeVisible();
+    await page.click('button:has-text("Fade Out!")');
+    await expect(animated).toHaveClass(/ng-leave/);
+    await expect(animated).toHaveClass(/ng-leave-active/);
+    await page.waitForTimeout(500);
+    // should be invisible
+    await expect(animated).not.toBeVisible();
 
-  // when if attribute has no animate antributes
-  await page.click('button:has-text("Fade In!")');
-  let nonAnimated = await page.locator("#no-animate");
+    // when if attribute has no animate antributes
+    await page.click('button:has-text("Fade In!")');
+    let nonAnimated = await page.locator("#no-animate");
 
-  await expect(nonAnimated).toBeVisible();
-  await expect(nonAnimated).not.toHaveClass(/ng-enter/);
-  await expect(nonAnimated).not.toHaveClass(/ng-enter-active/);
+    await expect(nonAnimated).toBeVisible();
+    await expect(nonAnimated).not.toHaveClass(/ng-enter/);
+    await expect(nonAnimated).not.toHaveClass(/ng-enter-active/);
 
-  // Wait for the transition to complete
-  await page.waitForTimeout(100);
+    // Wait for the transition to complete
+    await page.waitForTimeout(100);
 
-  await page.click('button:has-text("Fade Out!")');
-  // should be instantly invisible
-  await expect(nonAnimated).not.toBeVisible();
+    await page.click('button:has-text("Fade Out!")');
+    // should be instantly invisible
+    await expect(nonAnimated).not.toBeVisible();
+  });
+
+  test("should destroy the previous leave animation if a new one takes place", async ({
+    page,
+  }) => {
+    await page.goto("src/directive/if/if-animate-css.html");
+    await page.content();
+    // when if attribute has `data-animate` set to true
+    await page.click('button:has-text("Fade In!")');
+    let animated = await page.locator("#data-animate");
+
+    await expect(animated).toBeVisible();
+    await expect(animated).toHaveClass(/ng-enter/);
+    await expect(animated).toHaveClass(/ng-enter-active/);
+
+    // Wait for the transition to complete
+    await page.waitForTimeout(500);
+    await expect(animated).not.toHaveClass(/ng-enter/);
+    await expect(animated).not.toHaveClass(/ng-enter-active/);
+
+    await page.click('button:has-text("Fade Out!")');
+    await page.click('button:has-text("Fade In!")');
+    // should be visible
+    await page.waitForTimeout(500);
+    await expect(animated).toBeVisible();
+    await expect(animated).not.toHaveClass(/ng-leave/);
+    await expect(animated).not.toHaveClass(/ng-leave-active/);
+  });
+
+  test("should work with svg elements when the svg container is transcluded", async ({
+    page,
+  }) => {
+    await page.goto("src/directive/if/if-animate-css.html");
+    await page.content();
+    await page.click('button:has-text("Fade In!")');
+    await page.waitForTimeout(600);
+    let animated = await page.locator("#circle");
+    await expect(animated).not.toHaveClass(/ng-enter/);
+    await expect(animated).not.toHaveClass(/ng-enter-active/);
+  });
 });
