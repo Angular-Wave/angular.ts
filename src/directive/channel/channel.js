3a528290dc1be7b0c28e76f2854e787c3c984bd8
Anatoly Ostrovsky <anatolyostrovsky@gmail.com>
2025-03-10 00:50:37 +0200

    Doc updates

--- src/directive/channel/channel.js
+++ src/directive/channel/channel.js
@@ -6,11 +6,17 @@ import { isObject } from "../../shared/utils.js";
  * If data is sent via `EventBus` on the specified `ngChannel`, the directive attempts to update the element's content accordingly,
  * either by directly setting the inner HTML or merging the scope's data if the element contains a template.
  *
- * If the element has a template (i.e., child nodes), the directive will use `scope.$merge` to update the scope,
+ * If the element has a template and incoming data is an object, the directive will merge all key/value pairs onto the scope,
  * allowing Angular expressions (`{{ yourModel }}`) to be evaluated and rendered.
  *
  * When the scope is destroyed, the directive automatically unsubscribes from the channel.
+ * Example:
  *
+ * HTML:
+ * <div ng-channel="userChannel">Hello {{ user.firstName }} {{ user.lastName }}</div>
+ *
+ * JavaScript:
+ * angular.EventBus.publish('userChannel', { user: { firstName: 'John', lastName: 'Smith' } });
  *
  * @returns {import("../../types.js").Directive}
  */
