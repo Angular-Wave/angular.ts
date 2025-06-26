29320aa0c51a6ea3eb3692117418407804a31361
Anatoly Ostrovsky <aostrovsky@evolution.com>
2025-06-26 18:40:26 +0300

    Update type signature

--- @types/core/error-handler.d.ts
+++ @types/core/error-handler.d.ts
@@ -2,6 +2,6 @@
  * A callback type for handling errors.
  *
  * @param exception - The exception associated with the error.
- * @param cause - Optional information about the context in which the error was thrown.
+ * @param [cause] - Optional information about the context in which the error was thrown.
  */
 export type ErrorHandler = (exception: Error, cause?: string) => void;
